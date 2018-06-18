cafe96e9213531f2b425e97b1de1da2a4d93e150
tomlievesley <tomlievesley@hotmailco.uk>
2018-06-18 17:42:20 +0100

    - spoilers fixed

--- Spoilers.py
+++ Spoilers.py
@@ -45,7 +45,14 @@ def get_new_spoilers():
                 continue
             # send the card to facebook
             card_text = "SPOILER ALERT  - {}".format(card['name'])
-            send_image(card['image_uris']['normal'].split("?")[0], card_text)
+            print(card)
+            if card['layout'] == 'normal':
+                send_image(card['image_uris']['normal'].split("?")[0], card_text)
+            else:
+                #transform cards
+                if card['layout'] == 'transform':
+                    for face in card['card_faces']:
+                        send_image(face['image_uris']['normal'].split("?")[0], card_text)
             # send_image(card['image_uris']['normal'], card['name'])
 
         json_dict = {"spoilers": savelist,
@@ -63,13 +70,31 @@ def send_image(image, message):
 
     client = Client(cred_List["email"], cred_List["password"])
 
+    print(image)
+
     # Will download the image at the url `<image url>`, and then send it
-    client.sendRemoteImage(image, message=Message(text=message),
-                           thread_id=cred_List["spoiler_thread"], thread_type=thread_type)
+    # client.sendRemoteImage(image, message=Message(text=message),
+    #                        thread_id=cred_List["spoiler_thread"], thread_type=thread_type)
 
     client.logout()
     timestamp_text = "{0} - {1}".format(datetime.datetime.today(), message)
     print(timestamp_text)
+
+
+def send_text(card):
+    thread_type = ThreadType.GROUP
+
+    with open('Settings.json', 'r') as json_data:
+        d = json.load(json_data)
+        cred_List = d["credentials"]
+
+    client = Client(cred_List["email"], cred_List["password"])
+
+    card_message = "{}\n{}".format(card['name'], card['oracle_text'])
+
+    print(card_message)
+
+    # client.send(Message(text=card_message), thread_id=cred_List["spoiler_thread"], thread_type=thread_type)
 
 while True:
     get_new_spoilers()
